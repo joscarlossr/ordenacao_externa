d1cfb286d417288e6e55b003d630cdd85d13c45a
J Carlos <joscarlossr@gmail.com>
2022-09-26 11:01:10 -0300

    add text var on create_entry fun for generalization

--- ordenacao.py
+++ ordenacao.py
@@ -1,15 +1,17 @@
 import string
 
 
+text = "I N T E R C A L A C A O B A L A N C E A D A"
 alphabet = list(string.ascii_uppercase)
 
 
-def create_entry_file():
+def create_entry_file(text):
     """
     Generates input file (entrada.txt) based on the phrase given.
     """
-    frase = "I N T E R C A L A C A O B A L A N C E A D A".upper().split()
-    numbers = [str(alphabet.index(char)) for char in frase]
+    text = text.upper().split()
+
+    numbers = [str(alphabet.index(char)) for char in text]
 
     with open('entrada.txt', 'w') as file:
         print('\n'.join(numbers), file=file)
@@ -25,9 +27,10 @@ def get_lines(file_path, line_numbers):
     return (x for i, x in enumerate(file_path) if i in line_numbers)
 
 
-# create_entry_file()
+# create_entry_file(text)
 
 with open('entrada.txt', 'r') as file:
+    #TODO: #1 Solve cont variable, list parameter in get_lines function
     cont = 3
 
     lines = get_lines(file, list(range(cont, cont+3)))
