3cc5f02d38626ff40e875e5c8d591c39167433ab
J Carlos <joscarlossr@gmail.com>
2022-09-26 10:50:11 -0300

    creates functions documentation and minor modifs

--- ordenacao.py
+++ ordenacao.py
@@ -4,8 +4,11 @@ import string
 alphabet = list(string.ascii_uppercase)
 
 
-def arquivo_entrada():
-    frase = "I N T E R C A L A C A O B A L A N C E A D A".split()
+def create_entry_file():
+    """
+    Generates input file (entrada.txt) based on the phrase given.
+    """
+    frase = "I N T E R C A L A C A O B A L A N C E A D A".upper().split()
     numbers = [str(alphabet.index(char)) for char in frase]
 
     with open('entrada.txt', 'w') as file:
@@ -13,10 +16,16 @@ def arquivo_entrada():
 
 
 def get_lines(file_path, line_numbers):
+    """
+    Returns a tuple with the lines contents based on the index given (list)
+        in "line_numbers".
+
+    file_path: path to the file with the content.
+    """
     return (x for i, x in enumerate(file_path) if i in line_numbers)
 
 
-# arquivo_entrada()
+# create_entry_file()
 
 with open('entrada.txt', 'r') as file:
     cont = 3
