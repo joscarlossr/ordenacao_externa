30f65f33c57a0fc8ec90069732dc66b3b20da9f4
J Carlos <joscarlossr@gmail.com>
2022-09-27 07:52:18 -0300

    segunda passada

--- ordenacao.py
+++ ordenacao.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 from typing import IO
+from math import ceil
 
 
 def swap(input_list: list, first_index: int, second_index: int):
@@ -22,6 +23,26 @@ def order_list(input_list: list):
         swap(input_list, 0, 2)
     if input_list[1] > input_list[2]:
         swap(input_list, 1, 2)
+
+
+def sort_buffer(keys: list, files: list):
+    tamanho = len(keys)
+    if tamanho <= 1:
+        return keys, files
+    if tamanho == 2:
+        if keys[0] > keys[1]:
+            swap(keys, 0, 1)
+            swap(files, 0, 1)
+        return keys, files
+    if keys[0] > keys[1]:
+        swap(keys, 0, 1)
+        swap(files, 0, 1)
+    if keys[0] > keys[2]:
+        swap(keys, 0, 2)
+        swap(files, 0, 2)
+    if keys[1] > keys[2]:
+        swap(keys, 1, 2)
+        swap(files, 1, 2)
 
 
 def clear_folder(folder: str):
@@ -53,11 +74,14 @@ if __name__ == '__main__':
             open(f'{results_folder}/fita1.txt', 'w'),
             open(f'{results_folder}/fita2.txt', 'w'),
             open(f'{results_folder}/fita3.txt', 'w'),
+            open(f'{results_folder}/fita4.txt', 'w'),
+            open(f'{results_folder}/fita5.txt', 'w'),
+            open(f'{results_folder}/fita6.txt', 'w'),
         ]
 
         i = 0
         index = 0
-        for line in f:
+        for idx, line in enumerate(f):
             list_aux.append(int(line.strip()))
             if len(list_aux) % 3 != 0:
                 continue
@@ -70,9 +94,39 @@ if __name__ == '__main__':
             i += 1
 
         if len(list_aux) > 0:
-            write_to_file(file_handles[index+1], list_aux)
+            write_to_file(file_handles[index % 3 + 1], list_aux)
 
-    # Fecha todos arquivos, e ao mesmo tempo, abre outro handle para leitura
+        # total de colunas na primeira passada
+        columns = ceil((idx+1) / 9)
+
+        # Fecha todos arquivos, e ao mesmo tempo abre outro handle para leitura
         for i in range(3):
             file_handles[i].close()
             file_handles[i] = open(file_handles[i].name, 'r')
+
+    keys_buffer, files_buffer = [], []
+    current_file = 4
+    for col in range(columns):
+        count = 0
+        # pega uma chave de cada file (3 chaves)
+        for i in range(3):
+            key = next(file_handles[i], None)
+            keys_buffer.append(int(key.strip()))
+            files_buffer.append(i)
+
+        # FIXME: #2 Trocar numeros para variaveis como este nove a seguir
+        while count < 9:
+            sort_buffer(keys_buffer, files_buffer)
+            print(f"current_file: {current_file}, count: {count}, keys: {keys_buffer}, files:{files_buffer}")
+            write_to_file(file_handles[current_file], list([keys_buffer[0]]))
+
+            # retorna a proxima chave do arquivo contendo a atual menor chave
+            key = next(file_handles[files_buffer[0]], None)
+            if key:
+                keys_buffer[0] = int(key.strip())
+                count += 1
+
+            keys_buffer.clear(), files_buffer.clear()
+            break
+
+        current_file += 1
