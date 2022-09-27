ee3f534abe766f0d26a721d9bcad8edf1c21e5ec
Tarcisio <60912483+tcortega@users.noreply.github.com>
2022-09-27 02:17:54 -0300

    rework

--- ordenacao.py
+++ ordenacao.py
@@ -1,38 +1,76 @@
-import string
+import os
+import shutil
+from typing import IO
 
 
-text = "I N T E R C A L A C A O B A L A N C E A D A"
-alphabet = list(string.ascii_uppercase)
+def swap(input_list: list, first_index: int, second_index: int):
+    val_primeiro = input_list[first_index]
+    val_segundo = input_list[second_index]
+    input_list[first_index] = val_segundo
+    input_list[second_index] = val_primeiro
 
 
-def create_entry_file(text):
-    """
-    Generates input file (entrada.txt) based on the phrase given.
-    """
-    text = text.upper().split()
-
-    numbers = [str(alphabet.index(char)) for char in text]
-
-    with open('entrada.txt', 'w') as file:
-        print('\n'.join(numbers), file=file)
-
-
-def get_lines(file_path, line_numbers):
-    """
-    Returns a tuple with the lines contents based on the index given (list)
-        in "line_numbers".
-
-    file_path: path to the file with the content.
-    """
-    return (x for i, x in enumerate(file_path) if i in line_numbers)
+def order_list(input_list: list):
+    tamanho = len(input_list)
+    if tamanho <= 1:
+        return input_list
+    if tamanho == 2:
+        return [min(input_list), max(input_list)]
+    if input_list[0] > input_list[1]:
+        swap(input_list, 0, 1)
+    if input_list[0] > input_list[2]:
+        swap(input_list, 0, 2)
+    if input_list[1] > input_list[2]:
+        swap(input_list, 1, 2)
 
 
-# create_entry_file(text)
+def clear_folder(folder: str):
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Falha ao deletar arquivo %s. Motivo: %s' % (file_path, e))
 
-with open('entrada.txt', 'r') as file:
-    #TODO: #1 Solve cont variable, list parameter in get_lines function
-    cont = 3
 
-    lines = get_lines(file, list(range(cont, cont+3)))
-    for line in lines:
-        print(line.strip())
+def write_to_file(handle: IO, input_list: list):
+    stringified = [str(x) for x in input_list]
+    handle.write('\n'.join(stringified) + '\n')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    results_folder = './resultados'
+    clear_folder(results_folder)
+
+    with open('entrada.txt') as f:
+        list_aux = []
+        ordered_lists = []
+        file_handles = [
+            open(f'{results_folder}/fita1.txt', 'w'),
+            open(f'{results_folder}/fita2.txt', 'w'),
+            open(f'{results_folder}/fita3.txt', 'w'),
+        ]
+
+        i = 0
+        for line in f:
+            list_aux.append(int(line.strip()))
+            if len(list_aux) % 3 != 0: continue
+
+            order_list(list_aux)
+            index = i % 3
+            write_to_file(file_handles[index], list_aux)
+
+            list_aux.clear()
+            i += 1
+
+        if len(list_aux) > 0:
+            write_to_file(file_handles[-1], list_aux)
+
+        # Fecha todos arquivos, e ao mesmo tempo, abre outro handle para leitura
+        for i in range(3):
+            file_handles[i].close()
+            file_handles[i] = open(file_handles[i].name, 'r')
